05780727889ee24870892490ee183e342814adf8
André Martins <objetovazio@gmail.com>
2018-04-18 10:57:50 -0300

    att dao

--- Padroes_de_Projeto/DAO/HibernateDAO/src/main/java/example/hibernate/persistencia/VeiculoDAOImpl.java
+++ Padroes_de_Projeto/DAO/HibernateDAO/src/main/java/example/hibernate/persistencia/VeiculoDAOImpl.java
@@ -3,6 +3,7 @@
 package example.hibernate.persistencia;
 
 import example.hibernate.model.Veiculo;
+import java.util.List;
 
 
 
@@ -12,6 +13,13 @@ import example.hibernate.model.Veiculo;
  */
 public class VeiculoDAOImpl  extends GenericDAOImpl<Veiculo> implements VeiculoDAO {
 
-        
+        public List<Veiculo> buscaVeiculosApartirDoAno(int ano){            
+            sessao = HibernateUtil.getSession();
+            transacao = sessao.beginTransaction();                        
+            List<Veiculo> lista = sessao.createQuery("from Veiculo where ano >= " + ano).list();
+            transacao.commit();            
+            sessao.close();
+            return lista;
+        }
     
 }
