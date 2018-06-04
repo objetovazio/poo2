d0cb4849cb8868246190e135bf99d1dfecc9ae44
André Martins <objetovazio@gmail.com>
2018-06-04 11:33:56 -0300

    Starting Padrao Template

---
+++ Padroes_de_Projeto/Comportamental/Template/TemplateExportFiles/src/templateexemplo/AbstractExport.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templateexemplo;
+
+/**
+ *
+ * @author andre
+ */
+public abstract class AbstractExport {
+    public abstract void export(String txt);
+    protected abstract void printLine(String linha);
+
+}
